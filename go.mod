a5dec0b7632d435e11b27d149f15e9ed62802cd7
jonaaldas <jonaaldas@gmail.com>
2024-03-02 22:30:11 -0500

    new files

---
+++ go.mod
@@ -0,0 +1,15 @@
+module cron-db
+
+go 1.21.1
+
+require (
+	github.com/go-co-op/gocron/v2 v2.2.4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/jonboulle/clockwork v0.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	golang.org/x/exp v0.0.0-20231219180239-dc181d75b848 // indirect
+	gopkg.in/robfig/cron.v2 v2.0.0-20150107220207-be2e0b0deed5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"gopkg.in/robfig/cron.v2"
+)
+
+type RestaurantId struct {
+	ID int `db:"id"`
+}
+
+func connectToDB() {
+	user := goDotEnvVariable("DB_USER")
+	dataBaseName := goDotEnvVariable("DATA_BASE_NAME")
+	password := goDotEnvVariable("PASSWORD")
+	host := goDotEnvVariable("HOST")
+	database := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=disable", user, dataBaseName, password, host)
+
+	db, err := sqlx.Connect("postgres", database)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("Successfully Connected")
+	}
+
+	var restaurantId RestaurantId
+	err = db.Get(&restaurantId, "SELECT id FROM restaurants LIMIT 1")
+	if err != nil {
+		log.Fatal("Failed to query restaurant ID:", err)
+	}
+
+	fmt.Printf("First Restaurant ID: %d\n", restaurantId.ID)
+	fmt.Printf("I finish running")
+
+}
+
+func runCronJobs() {
+	fmt.Print("I was running")
+	s := cron.New()
+	s.AddFunc("* * * * *", func() {
+		connectToDB()
+	})
+	s.Start()
+}
+
+func goDotEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+func main() {
+	runCronJobs()
+}
